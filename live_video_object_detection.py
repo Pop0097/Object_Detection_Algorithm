d6c69a82a5fce6ed3d6b643300c9c2a1a1e3dffd
Dhruv Rawat <dhruvrawat09@gmail.com>
2020-10-01 22:55:04 -0400

    Object detection in video is improved.

--- live_video_object_detection.py
+++ live_video_object_detection.py
@@ -1,27 +1,33 @@
 import cv2
 import numpy as np
 
+### METHOD BLOCK BEGINS ###
+
 def detect_object():
     # Arrays store the minimum and maximum values for each HSV value. Refer to "hsv_value_setup.py"
     minimum_values = np.array([7, 0, 0])
-    maximum_values = np.array([179, 255, 255])
+    maximum_values = np.array([150, 255, 255])
 
     image_HSV = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)  # Converts the image from blue green red (BGR) to HSV
     image_HSV_specific = cv2.inRange(image_HSV, minimum_values,
                                      maximum_values)  # Converts HSV image so only the red ball is displayed
-    contours, hierarchy = cv2.findContours(image_HSV_specific, cv2.RETR_LIST,
+    blur_image = cv2.blur(image_HSV_specific, (5, 5))
+    contours, hierarchy = cv2.findContours(blur_image, cv2.RETR_LIST,
                                            cv2.CHAIN_APPROX_SIMPLE)  # Finds contours in image (which can be used to identify shapes)
 
     for contour in contours:
         shape_area = cv2.contourArea(contour)
-        if shape_area > 500:  # If shape area is larger than 500 (makes sure random detectoins are not processed)
+        if shape_area > 1000:  # If shape area is larger than 500 (makes sure random detectoins are not processed)
             perimeter = cv2.arcLength(contour, True)
             verticies = cv2.approxPolyDP(contour, 0.03 * perimeter, True)  # Adjust values as needed
 
-            if len(verticies) > 5:  # Circles will still have verticies. If we can find a good balance then we are solid
+            if len(verticies) > 6:  # Circles will still have verticies. If we can find a good balance then we are solid
                 x, y, width, height = cv2.boundingRect(verticies)  # Gets parameters for a rectangle around object
                 cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 3)  # Draws rectangle on image
 
+### HELPER BLOCK ENDS ###
+
+### MAIN CODE BLOCK BEGINS ###
 
 # Video frame dimensions
 video_frame_width = 640
@@ -36,10 +42,9 @@ video_capture.set(10, video_brightness_adjustment)
 
 
 while True:
-
     success, image = video_capture.read()
 
-    x, y, width, height = detect_object() # Calls method to process image and identify object
+    detect_object() # Calls method to process image and identify object
 
     cv2.imshow("Webcam_Input", image)
 
@@ -48,4 +53,6 @@ while True:
 
 
 print("Terminated")
+
+### MAIN CODE BLOCK ENDS ###
 
